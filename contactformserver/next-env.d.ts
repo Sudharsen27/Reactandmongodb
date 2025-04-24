775bc0b3c29b49e3962273bd0a96d3f1d7d296b0
Sudharsen27 <sundarlingam272000@gmail.com>
2025-04-24 13:31:26 +0530

    mongodb

---
+++ contactformserver/next-env.d.ts
@@ -0,0 +1,4 @@
+// Move this to an .env file or wrap it in a string if used in TypeScript
+const MONGODB_URI = "mongodb+srv://your_username:your_password@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority";
+//         <a
+//           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
